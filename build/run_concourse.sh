2d65ca4adb8a21fff060dbf5c131e6f1ada63516
Adham Helal <aa@hellofresh.com>
2017-01-05 11:08:28 +0100

    remove daemon

--- build/run_concourse.sh
+++ build/run_concourse.sh
@@ -5,8 +5,9 @@ MY_PATH="`dirname \"$0\"`"          # relative
 DIR="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
 
 echo "run docker"
-/etc/init.d/docker start
-
+#/etc/init.d/docker start
+#wrapdocker&
+#sleep 5
 echo "Done sleeping"
 
 "${DIR}"/run_kitchen.sh "${1}"
