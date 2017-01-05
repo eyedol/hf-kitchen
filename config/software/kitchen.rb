3cd10fd30a4180fa34935ba6b36fd11bda6e5410
Adham Helal <aa@hellofresh.com>
2017-01-05 21:05:48 +0100

    Gem install serverspec, bundler and net-ssh

--- config/software/kitchen.rb
+++ config/software/kitchen.rb
@@ -8,14 +8,21 @@ dependency 'ruby'
 dependency 'rubygems'
 
 build do
+  ## TODO: WE should pin plugins
   env = with_standard_compiler_flags(with_embedded_path)
-  # TODO: WE should pin plugins
-  gem 'install kitchen-vagrant'
-  gem 'install kitchen-ec2'
-  gem 'install kitchen-docker'
-  gem 'install kitchen-ansiblepush'
-  gem 'install kitchen-goss'
-  gem 'install kitchen-verifier-serverspec'
+  ## Kitchen plugins
+  gem 'install kitchen-localhost --no-ri --no-rdoc'
+  gem 'install kitchen-vagrant --no-ri --no-rdoc'
+  gem 'install kitchen-ec2 --no-ri --no-rdoc'
+  gem 'install kitchen-docker --no-ri --no-rdoc'
+  gem 'install kitchen-ansiblepush --no-ri --no-rdoc'
+  gem 'install kitchen-goss --no-ri --no-rdoc'
+  gem 'install kitchen-verifier-serverspec --no-ri --no-rdoc'
+  ## Requirements
+  gem 'install bundler --no-ri --no-rdoc'
+  gem 'install serverspec --no-ri --no-rdoc'
+  gem 'install net-ssh --no-ri --no-rdoc'
+  ## Build test-kitchen
   gem 'build test-kitchen.gemspec'
   gem "install test-kitchen-*.gem --no-ri --no-rdoc --bindir '#{install_dir}/bin'", env: env
 end
