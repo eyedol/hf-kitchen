969ed841d6eefe17b77909226cf6ee87c2f00541
Adham Helal <aa@hellofresh.com>
2017-01-05 10:09:58 +0100

    don’t use wrapper script

--- build/run_concourse.sh
+++ build/run_concourse.sh
@@ -3,9 +3,8 @@ MY_PATH="`dirname \"$0\"`"          # relative
 DIR="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
 
 echo "run docker"
-wrapdocker&
+/etc/init.d/docker start
 
-sleep 4
 echo "Done sleeping"
 
 "${DIR}"/run_kitchen.sh "${1}"
